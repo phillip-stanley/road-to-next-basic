76ca2805a5bbca645eee03d4ad09e58d4db62a47
phillip stanley <phillip.stanley39@gmail.com>
2025-12-23 22:10:05 +0000

    fix: update theme switcher to avoid hydration issues

--- src/components/theme/theme-switcher.tsx
+++ src/components/theme/theme-switcher.tsx
@@ -14,7 +14,18 @@ const ThemeSwitcher = () => {
             onClick={() => setTheme(theme === "light" ? "dark" : "light")}
             className="cursor-pointer w-6 h-6"
         >
-            {theme == "light" ? <LucideMoon /> : <LucideSun />}
+            <LucideSun
+                className="
+                    h-4 w-4 rotate-0 scale-100 transition-all
+                    dark:-rotate-90 dark:scale-0
+                "
+            />
+            <LucideMoon
+                className="
+                    absolute h-4 w-4 rotate-90 scale-0 transition-transform
+                    dark:rotate-0 dark:scale-100
+                "
+            />
             <span className="sr-only">Toggle theme</span>
         </Button>
     )
